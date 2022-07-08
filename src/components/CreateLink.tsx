8503b75d05300e3e0aa8338800c104074e608f47
Mogakamo <mogaka.amo254@gmail.com>
2022-07-08 12:59:19 +0300

    commit

--- src/components/CreateLink.tsx
+++ src/components/CreateLink.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useMutation, gql } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
 
 const CREATE_LINK_MUTATION = gql`
     mutation PostMutation(
@@ -16,6 +17,7 @@ const CREATE_LINK_MUTATION = gql`
 `;
 
 const CreateLink = () => {
+    const navigate = useNavigate();
   const [formState, setFormState] = useState({
     description: "",
     url: "",
@@ -26,6 +28,7 @@ const CreateLink = () => {
         description: formState.description,
         url: formState.url,
     },
+    onCompleted: () => navigate("/"),
   });
 
   return (
