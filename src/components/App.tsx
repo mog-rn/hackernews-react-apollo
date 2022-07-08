75b6dee3ea0bf1ff872bd37a787734f2f9d40bb6
Mogakamo <mogaka.amo254@gmail.com>
2022-07-08 13:36:11 +0300

    commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 import Header from './Header'
 import { Route, Routes } from 'react-router-dom'
+import Login from './Login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LinkList />} />
           <Route path="/create" element={<CreateLink />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </div>
---
+++ src/components/Login.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [formState, setFormState] = useState({
+    login: true,
+    email: "",
+    password: "",
+    name: "",
+  });
+
+  return (
+    <div>
+      {formState.login ? "Login" : "Sign Up"}
+      {!formState.login && (
+        <>
+          <input
+            type="text"
+            value={formState.name}
+            onChange={(e) =>
+              setFormState({ ...formState, name: e.target.value })
+            }
+            placeholder="Your name"
+          />
+          <input
+            type="text"
+            value={formState.email}
+            onChange={(e) =>
+              setFormState({ ...formState, email: e.target.value })
+            }
+            placeholder="Your email address"
+          />
+          <input
+            type="password"
+            value={formState.password}
+            onChange={(e) =>
+              setFormState({ ...formState, password: e.target.value })
+            }
+            placeholder="Choose a safe Password"
+          />
+          <button onClick={() => console.log("onClick")}>
+            {formState ? "login" : "Create an account"}
+          </button>
+          <button
+            onClick={(e) =>
+              setFormState({ ...formState, login: !formState.login })
+            }
+          >
+            {formState.login
+              ? "need to create an account"
+              : "already have an account?"}
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Login;
