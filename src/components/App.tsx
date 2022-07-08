40cdca5438cf8a6c12e6fe0ff3c685047a85ea71
Mogakamo <mogaka.amo254@gmail.com>
2022-07-08 11:19:38 +0300

    Link component is displaying

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import LinkList from './LinkList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       Hackernews React Apollo
+      <LinkList />
     </div>
   )
 }
