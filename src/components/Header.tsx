c56f9f9c6e93f4d2e0605797d0d29c4235d793f6
Mogakamo <mogaka.amo254@gmail.com>
2022-07-08 14:21:59 +0300

    commt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AUTH_TOKEN } from "../constants";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const authToken = localStorage.getItem(AUTH_TOKEN);
   return (
     <div>
-        <Link to="/">Hacker News</Link>
-        <Link to="/">new</Link>
-        <Link to="/create">Submit</Link>
+      <Link to="/">Hacker News</Link>
+      <Link to="/">new</Link>
+      <Link to="/create">Submit</Link>
+      <div>
+        {authToken ? (
+          <>
+            <div
+              onClick={() => {
+                localStorage.removeItem(AUTH_TOKEN);
+                navigate("/");
+              }}
+            >
+              logout
+            </div>
+          </>
+        ) : (
+          <>
+            <Link to="/login">login</Link>
+          </>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -13,46 +13,48 @@ const Login = () => {
   return (
     <div>
       {formState.login ? "Login" : "Sign Up"}
-      {!formState.login && (
-        <>
-          <input
-            type="text"
-            value={formState.name}
-            onChange={(e) =>
-              setFormState({ ...formState, name: e.target.value })
-            }
-            placeholder="Your name"
-          />
-          <input
-            type="text"
-            value={formState.email}
-            onChange={(e) =>
-              setFormState({ ...formState, email: e.target.value })
-            }
-            placeholder="Your email address"
-          />
-          <input
-            type="password"
-            value={formState.password}
-            onChange={(e) =>
-              setFormState({ ...formState, password: e.target.value })
-            }
-            placeholder="Choose a safe Password"
-          />
-          <button onClick={() => console.log("onClick")}>
-            {formState ? "login" : "Create an account"}
-          </button>
-          <button
-            onClick={(e) =>
-              setFormState({ ...formState, login: !formState.login })
-            }
-          >
-            {formState.login
-              ? "need to create an account"
-              : "already have an account?"}
-          </button>
-        </>
-      )}
+      <div>
+        {!formState.login && (
+          <>
+            <input
+              type="text"
+              value={formState.name}
+              onChange={(e) =>
+                setFormState({ ...formState, name: e.target.value })
+              }
+              placeholder="Your name"
+            />
+            <input
+              type="text"
+              value={formState.email}
+              onChange={(e) =>
+                setFormState({ ...formState, email: e.target.value })
+              }
+              placeholder="Your email address"
+            />
+            <input
+              type="password"
+              value={formState.password}
+              onChange={(e) =>
+                setFormState({ ...formState, password: e.target.value })
+              }
+              placeholder="Choose a safe Password"
+            />
+            <button onClick={() => console.log("onClick")}>
+              {formState ? "login" : "Create an account"}
+            </button>
+            <button
+              onClick={(e) =>
+                setFormState({ ...formState, login: !formState.login })
+              }
+            >
+              {formState.login
+                ? "need to create an account"
+                : "already have an account?"}
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
